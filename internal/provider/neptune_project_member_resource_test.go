31ff1c25c82870debadc445f2ac258168e6cff78
AleksanderWWW <alwojnarowicz@gmail.com>
2023-11-30 18:18:39 +0100

    add acceptance test for project member resource

---
+++ internal/provider/neptune_project_member_resource_test.go
@@ -0,0 +1,54 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccNeptuneProjectMemberResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: providerConfig + `
+				resource "neptune_project_member" "test_member" {
+					project   = "e2e-20231130-0854-mouth-southern-trip"
+					workspace = "e2e-tests"
+					username  = "e2e.regularuser"
+					role      = "member"
+				  }
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("neptune_project_member.test_member", "project", "e2e-20231130-0854-mouth-southern-trip"),
+					resource.TestCheckResourceAttr("neptune_project_member.test_member", "workspace", "e2e-tests"),
+					resource.TestCheckResourceAttr("neptune_project_member.test_member", "username", "e2e.regularuser"),
+					resource.TestCheckResourceAttr("neptune_project_member.test_member", "role", "member"),
+				),
+			},
+			// ImportState testing (not applicable)
+			// {
+			// 	ResourceName:      "neptune_project.test",
+			// 	ImportState:       true,
+			// 	ImportStateVerify: true,
+			// 	// This is not normally necessary, but is here because this
+			// 	// example code does not have an actual upstream service.
+			// 	// Once the Read method is able to refresh information from
+			// 	// the upstream service, this can be removed.
+			// },
+			// Update and Read testing (not applicable)
+			// {
+			// 	Config: testAccExampleResourceConfig("two"),
+			// 	Check: resource.ComposeAggregateTestCheckFunc(
+			// 		resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "two"),
+			// 	),
+			// },
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
