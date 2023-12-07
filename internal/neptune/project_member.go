69cde716e3d197ccbe83e818931bdf408874abd9
AleksanderWWW <alwojnarowicz@gmail.com>
2023-12-07 12:40:55 +0100

    add method to update project member permission

--- internal/neptune/project_member.go
+++ internal/neptune/project_member.go
@@ -3,6 +3,7 @@ package neptune
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -110,5 +111,75 @@ func (c *NeptuneClient) DeleteProjectMember(project string, workspace string, us
 	if resp.StatusCode != 200 {
 		return fmt.Errorf("Error: response status code %d", resp.StatusCode)
 	}
+	return nil
+}
+
+func (c *NeptuneClient) UpdateProjectMember(project string, workspace string, username string, role string) error {
+	role = strings.ToLower(role)
+	if _, ok := map[string]bool{
+		"viewer": true,
+		"member": true,
+		"owner":  true,
+	}[role]; !ok {
+		return fmt.Errorf("Unexpected 'role' argument value: '%s'", role)
+	}
+
+	authToken, err := c.getAuthToken()
+	if err != nil {
+		return err
+	}
+
+	projectIdentifier := fmt.Sprintf("%s/%s", workspace, project)
+
+	headers := map[string]string{
+		"authorization": fmt.Sprintf("Bearer %s", authToken),
+	}
+
+	params := map[string]string{
+		"projectIdentifier": projectIdentifier,
+	}
+
+	type reqBody struct {
+		Role string `json:"role"`
+	}
+
+	body := reqBody{
+		Role: role,
+	}
+
+	bodyJson, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	var opData operationData
+	err = viper.UnmarshalKey("updateProjectMember", &opData)
+	if err != nil {
+		return err
+	}
+	endpoint := opData.Endpoint + "/" + username
+
+	req, err := prepareRequest(c.creds.tokenOriginAddress, endpoint, opData.Method, params, headers, bodyJson)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == 404 {
+		return fmt.Errorf("Project '%s' not found", projectIdentifier)
+	}
+
+	if resp.StatusCode == 400 {
+		return fmt.Errorf("User '%s' not found", username)
+	}
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("Error: response status code %d", resp.StatusCode)
+	}
+
 	return nil
 }
